9b11ea5a0efd3be2f506f0664104052a3241a5b6
alicomert <alicmrt648@gmail.com>
2026-06-04 14:13:22 +0000

    Polish start menu layout and localization

--- scripts/StartMenu.gd
+++ scripts/StartMenu.gd
@@ -8,11 +8,18 @@ signal game_started
 @onready var credits_panel: VBoxContainer = %CreditsPanel
 @onready var title_label: Label = %TitleLabel
 @onready var start_button: Button = %StartButton
+@onready var continue_button: Button = main_panel.get_node("ContinueButton") as Button
 @onready var settings_button: Button = %SettingsButton
 @onready var credits_button: Button = %CreditsButton
 @onready var quit_button: Button = %QuitButton
 @onready var back_from_settings_button: Button = %BackFromSettingsButton
 @onready var back_from_credits_button: Button = %BackFromCreditsButton
+@onready var settings_title_label: Label = settings_panel.get_node("SettingsTitle") as Label
+@onready var language_label: Label = settings_panel.get_node("LanguageLabel") as Label
+@onready var language_select: OptionButton = settings_panel.get_node("LanguageSelect") as OptionButton
+@onready var brightness_label: Label = settings_panel.get_node("BrightnessLabel") as Label
+@onready var music_label: Label = settings_panel.get_node("MusicLabel") as Label
+@onready var sfx_label: Label = settings_panel.get_node("SfxLabel") as Label
 @onready var brightness_slider: HSlider = %BrightnessSlider
 @onready var music_slider: HSlider = %MusicSlider
 @onready var sfx_slider: HSlider = %SfxSlider
@@ -24,9 +31,44 @@ signal game_started
 
 var started := false
 var drift := 0.0
+var locale := "en"
+
+const LANGUAGES := [
+	{"code": "en", "label": "English"},
+	{"code": "tr", "label": "Turkce"},
+]
+
+const COPY := {
+	"en": {
+		"new_game": "NEW GAME",
+		"continue": "CONTINUE",
+		"settings": "SETTINGS",
+		"credits": "CREDITS",
+		"quit": "QUIT",
+		"language": "LANGUAGE",
+		"brightness": "BRIGHTNESS",
+		"music": "MUSIC",
+		"sfx": "SFX",
+		"back": "BACK",
+	},
+	"tr": {
+		"new_game": "YENI OYUN",
+		"continue": "DEVAM ET",
+		"settings": "AYARLAR",
+		"credits": "EMEK VERENLER",
+		"quit": "CIKIS",
+		"language": "DIL",
+		"brightness": "PARLAKLIK",
+		"music": "MUZIK",
+		"sfx": "EFEKTLER",
+		"back": "GERI",
+	},
+}
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	_setup_language_select()
+	_apply_locale(locale)
 	_show_panel(main_panel)
 	_connect_button(start_button, _on_start_pressed)
 	_connect_button(settings_button, func() -> void: _show_panel(settings_panel))
@@ -34,6 +76,7 @@ func _ready() -> void:
 	_connect_button(quit_button, _on_quit_pressed)
 	_connect_button(back_from_settings_button, func() -> void: _show_panel(main_panel))
 	_connect_button(back_from_credits_button, func() -> void: _show_panel(main_panel))
+	language_select.item_selected.connect(_on_language_selected)
 	brightness_slider.value_changed.connect(_on_brightness_changed)
 	music_slider.value_changed.connect(_on_music_changed)
 	sfx_slider.value_changed.connect(_on_sfx_changed)
@@ -66,7 +109,38 @@ func _show_panel(panel: Control) -> void:
 		focus_target = back_from_settings_button
 	elif panel == credits_panel:
 		focus_target = back_from_credits_button
-	focus_target.grab_focus.call_deferred()
+	focus_target.call_deferred("grab_focus")
+
+func _setup_language_select() -> void:
+	language_select.clear()
+	for index in range(LANGUAGES.size()):
+		var language: Dictionary = LANGUAGES[index]
+		language_select.add_item(language["label"], index)
+		language_select.set_item_metadata(index, language["code"])
+		if language["code"] == locale:
+			language_select.select(index)
+
+func _on_language_selected(index: int) -> void:
+	var next_locale := str(language_select.get_item_metadata(index))
+	_apply_locale(next_locale)
+
+func _apply_locale(next_locale: String) -> void:
+	if !COPY.has(next_locale):
+		next_locale = "en"
+	locale = next_locale
+	var copy: Dictionary = COPY[locale]
+	start_button.text = copy["new_game"]
+	continue_button.text = copy["continue"]
+	settings_button.text = copy["settings"]
+	credits_button.text = copy["credits"]
+	quit_button.text = copy["quit"]
+	settings_title_label.text = copy["settings"]
+	language_label.text = copy["language"]
+	brightness_label.text = copy["brightness"]
+	music_label.text = copy["music"]
+	sfx_label.text = copy["sfx"]
+	back_from_settings_button.text = copy["back"]
+	back_from_credits_button.text = copy["back"]
 
 func _on_start_pressed() -> void:
 	if started:
